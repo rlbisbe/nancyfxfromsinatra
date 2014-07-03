074e62f82a53c631ec47dec3c357e4284b3d63ac
Roberto Luis <rober90@gmail.com>
2014-07-03 14:01:02 +0200

    Added tests
    Not yet running...

--- NancyFxSinatra/Routes.cs
+++ NancyFxSinatra/Routes.cs
@@ -7,15 +7,16 @@ namespace NancyFxSinatra
 	{
 		public Routes ()
 		{
-			Get["/routes"] = _ => "Respuesta usando GET\n";
-			Post["/routes"] = _ => "Respuesta usando POST\n";
+			Get["/routes"] = _ => "Response with GET\n";
+			Post["/routes"] = _ => "Response with POST\n";
+			Put["/routes"] = _ => "Response with Put\n";
 
 			Get ["/actions/ok"] = _ => 202;
 			Get ["/actions/auth"] = _ => HttpStatusCode.Forbidden;
-			Get ["/actions/text"] = _ => "Hola Mundo";
+			Get ["/actions/text"] = _ => "Hello World";
 
 			Get["/actions/error"] = _ => {
-				Response res =  ((Response)"Error con mensaje\n");
+				Response res =  ((Response)"Error with message\n");
 				res.StatusCode = HttpStatusCode.InternalServerError;
 				return res;
 			};
---
+++ NancyFxSinatraTests/Test.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Text;
+using Nancy;
+using Nancy.Testing;
+
+namespace NancyFxSinatraTests
+{
+	/// <summary>
+	/// https://github.com/NancyFx/Nancy/wiki/Testing-your-application
+	/// </summary>
+
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestPut ()
+		{
+			var bootstrapper = new DefaultNancyBootstrapper();
+			var browser = new Browser(bootstrapper);
+
+			var result = browser.Put("/routes", with => {
+				with.HttpRequest();
+			});
+
+			Assert.AreEqual (HttpStatusCode.OK, result.StatusCode);
+			Assert.AreEqual ("Response with Put", result.Body);
+		}
+	}
+}
+
